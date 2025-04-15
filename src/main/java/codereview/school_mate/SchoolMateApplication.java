a3002211b3fec7c79cb1ad542523b79258bf2f20
fedeyak <fedeyak@gmail.com>
2025-04-15 17:04:56 +0300

    Empty project. Dependencies: - spring web - postgresql - liquibase - thymeleaf - lombok - mockito - spring security (disabled)

---
+++ src/main/java/codereview/school_mate/SchoolMateApplication.java
@@ -0,0 +1,13 @@
+package codereview.school_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMateApplication.class, args);
+	}
+
+}
