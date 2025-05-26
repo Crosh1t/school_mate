27ea298ff742bfe6c44fb36fe934e8a038e71e85
Crosh1t <40633165+Crosh1t@users.noreply.github.com>
2025-05-27 01:06:32 +0900

    Revert "feat/scool-mate-6"

--- src/main/resources/db/changelog/v-1.1-create_add_tables_with_links/create-table-teachers_subjects.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE teachers_subjects (
-     teacher_id BIGINT NOT NULL,
-     subject_id BIGINT NOT NULL,
-     PRIMARY KEY (teacher_id, subject_id),
-     CONSTRAINT fk_teacher_subject_teacher FOREIGN KEY (teacher_id) REFERENCES teachers(id),
-     CONSTRAINT fk_teacher_subject_subject FOREIGN KEY (subject_id) REFERENCES subject(id)
-);
