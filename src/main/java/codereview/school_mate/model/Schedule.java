e118b7f2aa58902fad881e0126d594de9938da79
Crosh1t <loookintome@gmail.com>
2025-06-14 00:31:40 +0900

    feat: The initial fields for the essence of the schedule have been added, to see the nuances of relationships, to add logic for days of the week and time.

--- src/main/java/codereview/school_mate/model/Schedule.java
+++ src/main/java/codereview/school_mate/model/Schedule.java
@@ -1,4 +1,48 @@
 package codereview.school_mate.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "schedule")
 public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "time", nullable = false)
+    private LocalTime time;
+
+    @Column(name = "description", nullable = true)
+    private StringBuilder description;
+
+    @Column(name = "day_of_week", nullable = false)
+    private Enum<DayOfWeek> dayOfWeekEnum;
+
+    @OneToMany(mappedBy = "parent", orphanRemoval = true, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<Subject> subjects;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "class_id", nullable = false)
+    private Set<SchoolClass> schoolClass;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id", nullable = false)
+    private Set<Teacher> teachers;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "classroom_id", nullable = false)
+    @JsonIgnore
+    private Classroom classroom;
 }
